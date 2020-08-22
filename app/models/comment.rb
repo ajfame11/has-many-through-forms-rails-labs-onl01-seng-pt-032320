51727e8c1106e7706301dd9e4738737c8020682f
AurelFontaine <ajfame11@gmail.com>
2020-08-22 13:32:29 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_attributes=(user_attributes)
+    self.user = User.find_or_create_by(username: user_attributes[:username]) if user_attributes[:username].present?
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,12 @@ class Post < ActiveRecord::Base
   has_many :users, through: :comments
 
 
+  def categories_attributes=(attributes)
+    attributes.values.each do |attribute|
+      if attribute[:name].present?
+        category = Category.find_or_create_by(attribute)
+        self.categories << category
+      end
+    end
+  end
 end
